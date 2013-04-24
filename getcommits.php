d1964eb1a004a39515cf58d60e371a32e434db44
Christian Vermeulen <info@christianvermeulen.net>
2013-04-24 10:09:46 +0200

    only get the latest 20 commits to start the stream of with and increase dn polling to every 5 seconds #mtfckr

--- getcommits.php
+++ getcommits.php
@@ -11,7 +11,7 @@ if($_REQUEST["last"] !== "0")
 {
     $result = $db->db->query("SELECT * FROM commits WHERE `created` > '".$_REQUEST['last']."' ORDER BY created ASC LIMIT 0,5");
 }else{
-    $result = $db->db->query("SELECT * FROM commits ORDER BY created ASC");
+    $result = $db->db->query("SELECT * FROM commits ORDER BY created DESC LIMIT 0,20");
 }
 
 while($row = $result->fetch_object())
